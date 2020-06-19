608d0f9b3e505d7b0b4782fc7287e32583b222b7
Nestor Infante <ninfante@mary-iancarina.com>
2020-06-19 16:30:50 -0400

    Modified file server

--- server.js
+++ server.js
@@ -4,31 +4,34 @@ const app = express()
 const bodyParse = require('body-parser')
 const { ApolloServer } = require('apollo-server-express')
 const PORT = process.env.PORT || 4100
-
+const fs = require('fs')
 const { loadSchemaBase } = require('./utils/loadScheme')
 const schemeBase = loadSchemaBase() // load scheme base GraphQL
 
 const servicePath = require('./servicesPath.json')
 
-// let 
+let Objects    = []
+let services   = []
+let keyService = []
+for (const key2 in servicePath) {
+    const pathService = Object.values(servicePath[key2])
+    
+    if( pathService[0] ) {
+        services = require(  pathService[0] )
 
-// for (const key2 in servicePath) {
-//     const pathService = Object.values(servicePath[key2])
-//     let keys = require(  pathService )
-//     console.log(keys)
-// }
+        keyService = Object.keys(servicePath[key2])
+        Objects[keyService] = services
+    }
 
-const services = {
-    login:  require('./services/login/resolvers'),
-    signUp: require('./services/signup/resolvers')
+   
 }
 
 let typeDefsd  = []
 let resolvers = []
 
-for (const key in services) {
-   typeDefsd  += services[key].typeDefs
-   resolvers  = services[key].resolvers
+for (const key in Objects) {
+   typeDefsd  += Objects[key].typeDefs
+   resolvers  = Objects[key].resolvers
 }
 
 const server = new ApolloServer({
